fb5f9c3c4c4cff91be83814d1b96e2b352499bd4
boyka <gxw255613@126.com>
2022-04-17 02:15:14 +0800

    Knife4j API文档管理

---
+++ common/src/main/java/com/ddoubuy/common/config/Knife4jConfig.java
@@ -0,0 +1,40 @@
+package com.ddoubuy.common.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @description: Knife4j API文档相关配置
+ * @author: boykaff
+ * @date: 2022-04-17-0017
+ */
+@Configuration
+@EnableSwagger2
+@EnableKnife4j
+public class Knife4jConfig {
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        .description("# swagger-bootstrap-ui-demo RESTful APIs")
+                        .contact(new Contact("宫兴伟", "", "gxw255613@126.com"))
+                        .version("1.0")
+                        .build())
+                //分组名称
+                .groupName("2.X版本")
+                .select()
+                //这里指定Controller扫描包路径
+                .apis(RequestHandlerSelectors.basePackage("com.ddoubuy"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+}
